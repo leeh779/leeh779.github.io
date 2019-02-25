7b0ffca4cc35358e85001807d3448dcc3da768a8
leeh779 <leeh779@newschool.edu>
2019-02-25 00:10:35 -0500

    maxim animation edit

--- projects/maxim/main.js
+++ projects/maxim/main.js
@@ -180,20 +180,30 @@ $(".quote19").click(function(){
    $('.quote3').fadeOut(350);
    $('.quote4').fadeOut(100);
    $('.quote5').delay(10).fadeOut(510);
-   $('.quote6').fadeOut(680);
+   $('.quote6').fadeOut(480);
    $('.quote7').fadeOut(250);
    $('.quote8').fadeOut(700);
    $('.quote9').fadeOut(420);
    $('.quote10').fadeOut(100);
+   $('.quote8').fadeIn(200);
    $('.quote11').delay(40).fadeOut(100);
    $('.quote12').fadeOut(300);
    $('.quote13').fadeOut(100);
+   $('.quote3').fadeIn(250);
    $('.quote14').fadeOut(700);
-   $('.quote15').fadeOut(850);
-   $('.quote16').delay(280).fadeOut(100);
+   $('.quote7').fadeIn(650);
+   $('.quote15').fadeOut(350);
+   $('.quote16').fadeOut(100);
    $('.quote17').fadeOut(200);
-   $('.quote18').fadeOut(590);
-   $('.quote19').fadeOut(620);
+   $('.quote9').fadeIn(450);
+   $('.quote18').fadeOut(190);
+   $('.quote13').fadeIn(220);
+   $('.quote19').fadeOut(240);
+   $('.quote3').fadeOut(150);
+   $('.quote9').fadeOut(190);
+   $('.quote13').fadeOut(290);
+   $('.quote8').fadeOut(300);
+   $('.quote7').fadeOut(250);
    $('.quote20').delay(1000).fadeIn(5000);
    /*$(".quote1, .quote2, .quote3, .quote4, .quote5, .quote6, .quote7, .quote8, .quote9, .quote10, .quote11, .quote12, .quote13, .quote14, .quote15, .quote16, .quote17, .quote18, .quote19").each(function(){
     $(this).fadeOut(2000);*/
