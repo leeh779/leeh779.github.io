821a57f274a61147c5a419faf89ee55b068782a8
leeh779 <leeh779@newschool.edu>
2019-03-27 19:52:07 -0400

    ama setup

---
+++ projects/ama/main.js
@@ -0,0 +1,29 @@
+$(function() {
+  // Your interactions go here
+  $(document).on('click', function(event) {
+  // All javascript event handlers give you information about the event type in the event argument.
+  console.log(event);
+
+  // event.preventDefault() will prevent the default click event from happening in the browser.
+  // This makes it so that clicking a link doesn't actually go to that link.
+  event.preventDefault();
+
+  // You can refer to items on the event object, such as the target, which represents
+  // the individual DOM element you clicked.
+  var $clickTarget = $(event.target);
+
+  $clickTarget.append('<img src="http://leeh.github.io/projects/ama/img/icon-1.png" frameBorder="0" allowFullScreen></iframe>');
+});
+
+$(document).on('click', function(event) {
+  event.preventDefault();
+
+  var $clickTarget = $(event.target);
+
+  $clickTarget.css({ transform: 'scale(' + randomNumber(0.5, 1.5) + ')' });
+
+  function randomNumber(min, max) {
+    return Math.random() * (max - min) + min;
+  }
+});
+});
