7cae180d99eb4ce45dbfafc9a0e4bcd1d9fb6d54
leeh779 <leeh779@newschool.edu>
2019-04-17 20:25:10 -0400

    json exercise-link style and add function in main js

--- projects/json-exercise/main.js
+++ projects/json-exercise/main.js
@@ -1,5 +1,9 @@
+$(function() {
+
 $.get('https://leeh779.github.io/api.json', function(data) {
   console.log(data);
 }).fail(function() {
   console.log('Error in GET request. Handle the error gracefully.');
+});
+
 });
