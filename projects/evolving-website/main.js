f96d7119d04e5570de53e186f8b149e20fe7540a
leeh779 <leeh779@newschool.edu>
2019-05-09 01:53:59 -0400

    organized class for js div

--- projects/evolving-website/main.js
+++ projects/evolving-website/main.js
@@ -38,7 +38,6 @@ $(document).ready(function() {
         }
     });
 
-
     $('#button').click(function(event) {
         var month = $("#input-1").val();
         var day = $("#input-2").val();
@@ -52,15 +51,15 @@ $(document).ready(function() {
         $('#container').toggleClass('display-none');
 
         $('body').css('background-color', '#d226'+year2);
-        $('<div style="z-index: 10; position: absolute; top:'+2*month+'vw; left:'+3*month+'vw; width: 0; height: 0; border-left: '+month+'vw solid transparent; border-right: '+month+'vw solid transparent; border-bottom: '+2*month+'vw solid black;"></div>').appendTo('body');
-        $('<div class="rotate3" style="z-index: 10; position: absolute; top:'+day+'%; border-radius: 50%; height: auto; background: URL(assets/imgs/bgpattern-5.png) repeat; border: 1px solid black; width:'+day+'%; padding-top:'+day+'%;"></div>').appendTo('body');
-        $('<div style="z-index: 10; position: absolute; top:'+0.5*year2+'%; left:'+0.5*year2+'%; width: 30%; height: '+day*10+'%; background: URL(assets/imgs/bgpattern-4.png); border: 10px solid #cccccc;"></div>').appendTo('body');
-        $('<div style="z-index: 1; position: absolute; top:0; left:'+year2+'%; width: 35%; height: 100%; background: URL(assets/imgs/bgpattern-1.png) repeat"></div>').appendTo('body');
-        $('<div style="z-index: 2; position: absolute: top:0; left:'+day+'%; width:'+200*0.5*month+'px; height:'+40*month+'px; background: URL(assets/imgs/bgpattern-2.png) repeat;"></div>').appendTo('body');
-        $('<div style="z-index: 2; position: absolute; top: 30% left:0; width: 100%; height: 20%; background: URL(assets/imgs/bgpattern-month-'+month+'.png) repeat;"></div>').appendTo('body');
-        $('<div style="z-index: 2; position: absolute; top: 60%; left: 20%; width: 25%; height: 30%; border: 10px solid black;"></div>').appendTo('body');
-        $('<div class="rotate1" style="z-index: 2; position: absolute; top: 20%; left: 60%; width: 20%; height: 40%; border: 10px solid white;"></div>').appendTo('body');
-        $('<div class="rotate2" style="z-index: 2; position: absolute; left: 75%; width: 4%; height: 5%; border: 10px solid black;"></div>').appendTo('body');
+        $('<div class="shape1 triangle-rotate" style="top:'+2*month+'vw; left:'+3*month+'vw; border-left: '+month+'vw solid transparent; border-right: '+month+'vw solid transparent; border-bottom: '+2*month+'vw solid black;"></div>').appendTo('body');
+        $('<div class="shape2 rotate3" style="top:'+day+'%; border: 1px solid black; width:'+day+'%; padding-top:'+day+'%;"></div>').appendTo('body');
+        $('<div id="draggable" class="shape3" style="top:'+0.5*year2+'%; left:'+0.5*year2+'%; height: '+day*10+'%;"></div>').appendTo('body');
+        $('<div class="shape4" style="left:'+year2*0.9+'%;"></div>').appendTo('body');
+        $('<div class="shape5" style="left:'+day+'%; width:'+200*0.5*month+'px; height:'+40*month+'px;"></div>').appendTo('body');
+        $('<div class="shape6" style="background: URL(assets/imgs/bgpattern-month-'+month+'.png) repeat;"></div>').appendTo('body');
+        $('<div class="shape7 rectangle-hover"></div>').appendTo('body');
+        $('<div class="shape8 rotate1"></div>').appendTo('body');
+        $('<div class="shape9 rotate2"></div>').appendTo('body');
     });
 
     $('#btn-refresh').click(function() {
